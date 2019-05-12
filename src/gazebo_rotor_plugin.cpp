87aa5b42d34711bcd300094e04bddcb0fa754a72
Vit Hanousek <vithanousek@seznam.cz>
2019-05-12 11:33:08 +0200

    Flapping angle on constant RPM (RPM driven by pitch channel)

--- src/gazebo_rotor_plugin.cpp
+++ src/gazebo_rotor_plugin.cpp
@@ -9,8 +9,9 @@
 #include <cmath>
 
 #define SLOW_CONST 0.1
-#define DEBUG_CONST 500
+#define DEBUG_CONST 300
 #define PI 3.141592654
+#define AIR_DENSITY 1.2041
 #define ToDeg(a) ((a)/2.0/PI*360.0)
 #define ToRad(a) ((a)/360.0*2.0*PI)
 
@@ -40,7 +41,13 @@ namespace gazebo
 		this->base_link = model->GetChildLink(_sdf->GetElement("base_link")->Get<std::string>());
         this->rotor_pos = _sdf->Get<Vector3d>("rotor_pos");
 		this->blade_length=_sdf->GetElement("blade_length")->Get<double>();
+        this->blade_width=_sdf->GetElement("blade_width")->Get<double>();
+        this->blade_weight=_sdf->GetElement("blade_weight")->Get<double>();
         this->delta_angle=ToRad(_sdf->GetElement("blade_delta")->Get<double>());
+        this->air_density=_sdf->Get<double>("air_density");
+        this->number_of_elements=_sdf->Get<int>("element_count");;
+        this->element_area=this->blade_width*this->blade_length/this->number_of_elements;
+
 		 
 		base_link->VisualId(_sdf->GetElement("left_blade_visual")->Get<std::string>(), this->leftBladeVisualID);
 		base_link->VisualId(_sdf->GetElement("right_blade_visual")->Get<std::string>(), this->rightBladeVisualID);
@@ -49,9 +56,11 @@ namespace gazebo
 
 		counter=0;
 		rotorAngle=0.0;
-		rotorOmega=PI/2;
-        flaping_angle=ToRad(-15);
+		rotorOmega=PI/4;
+        flaping_angle=ToRad(0);
 		lastUpdateTime =0.0;
+
+        pitch=0.0;
 
 	 	lbladeDefaultPos=Vector3d(0.0, -blade_length/2,0.0);
 		rbladeDefaultPos=Vector3d(0.0, blade_length/2,0.0);
@@ -102,7 +111,7 @@ namespace gazebo
         Quaterniond Rrot(Vector3d(0,0,1), rotorAngle);//otočení rotoru
         Quaterniond Rblade=Rdisc*Rrot;//otočení rotoru na hlavě
 
-        //TADY CHYBY přidat úhel mezi osou rotace a osou hlavy později -- je otazka zda je natočený správně
+        //TADY CHYBY přidat úhel mezi osou rotace a osou hlavy později 
         Quaterniond RbladeTotal=Rblade*Quaterniond(Vector3d(0,1,0),getFlapCorectionAngle(flaping_angle)); //otočení listu včetně flaping úhlu
 
         Vector3d omega=Rdisc*Vector3d(0,0,rotorOmega);
@@ -111,15 +120,60 @@ namespace gazebo
 		Vector3d upward=RbladeTotal*Vector3d(0,0,1);
         Vector3d wingCut=RbladeTotal*Vector3d(0,1,0);
 
-        double d=blade_length;
-        //for přes elementy
-        Vector3d vel=(Rblade*Vector3d(0,d,0)).Cross(omega)+rotorLinVel;        
-        Vector2d wingVel= Vector2d(vel.Dot(forward),vel.Dot(upward));
+        Vector3d bladeAirMoment(0,0,0);
+        for(int e=0;e<number_of_elements;e++)
+        {
+            double d=(e+0.5)/number_of_elements*blade_length;
+
+            Vector3d vel=(Rblade*Vector3d(0,d,0)).Cross(omega)+rotorLinVel;        
+            Vector2d airfoilVel= Vector2d(vel.Dot(forward),vel.Dot(upward));
+            
+            double airfoilVelocity=airfoilVel.Length();
+            /*if(airfoilVelocity< SLOW_CONST)
+            {
+                if(counter==DEBUG_CONST)
+                    gzdbg << "Element Too Slow \n";
+                continue;
+            }*/
+
+		    double alpha=-std::atan2(airfoilVel.Y(),airfoilVel.X());
+            double q=0.5*this->air_density*this->element_area*airfoilVelocity*airfoilVelocity;
+            double L=q*getCL(alpha);
+            double D=q*getCD(alpha);
+
+            Vector3d liftDirection = vel.Cross(wingCut).Normalize();
+            Vector3d liftForce=L*liftDirection;//tohle je kolmo na vítr, ne na list
+            Vector3d dragDirection= -(vel-vel.Dot(wingCut)).Normalize();
+            Vector3d dragForce=D*dragDirection;//tohle je po větru
+
+            bladeAirMoment+=(Rblade*Vector3d(0,d,0)).Cross(liftForce);
+            bladeAirMoment+=(Rblade*Vector3d(0,d,0)).Cross(dragForce);
+        }
+
+        //rozklad momentů do flap a ax - do lokalních
+        Vector3d discLocalMoment=Rblade.RotateVectorReverse(bladeAirMoment);//zatím je flapMoment X složka,
+                                                                            // ale tady se pak projeví odklon osy od disku
         
-        double alpha=-std::atan2(wingVel.Y(),wingVel.X());//úhel náběhu
+        //výpočet flap_angle ?? =>přímý výpočet by mohl oscilovat po každém kroku - několik iterací pro výpočet flap_angle???
+        double weightMoment=blade_length/2.0*blade_weight*9.81;
+        double odstrMomentMax=blade_weight*rotorOmega*rotorOmega*(blade_length/2.0)*(blade_length/2.0);//to je taky jen přibližné, zanedbávám cos u vzdalenosti od těžiště
+        double zlo=(discLocalMoment.X()-weightMoment)/odstrMomentMax;
 
-
-
+        double new_flap_angle=0;
+        if(zlo>=-0.25 && zlo<=0.25)//jen od 15 do 15 stupňů
+        {
+            new_flap_angle=std::asin(zlo);
+        }
+        else
+        {
+            if(zlo>0.25)
+                new_flap_angle=ToRad(15);
+            if(zlo<-0.25)
+                new_flap_angle=ToRad(-15);
+        }
+        
+        flaping_angle=new_flap_angle;//očekává konstantní omega
+        rotorOmega+=2*PI/50*rotadd;
 
 		rotorAngle+=timeStep*rotorOmega; //Euler integration of rotor angle
 
@@ -129,42 +183,6 @@ namespace gazebo
 						
 
 		UpdateRotorVisualPos();
-
-		
-     /*   if(linVel.Length()< SLOW_CONST)
-        {
-            if(counter==DEBUG_CONST)
-                gzdbg << "Too slow \n";
-            return;
-        }
-
-		const Pose3d &  pose=rotor_link->WorldPose();
-
-
-
-		Vector2d wingVel= Vector2d(linVel.Dot(forward),linVel.Dot(upward));
-        
-        double wingVelocity=wingVel.Length();
-        if(wingVelocity< SLOW_CONST)
-        {
-            if(counter==DEBUG_CONST)
-                gzdbg << "Wing Too Slow \n";
-            return;
-        }
-
-		double alpha=-std::atan2(wingVel.Y(),wingVel.X());
-        double q=0.5*AIR_DENSITY*wingVelocity*wingVelocity*AREA;
-        double L=q*getCL(alpha);
-        double D=q*getCD(alpha);
-
-        Vector3d liftDirection = linVel.Cross(wingCut).Normalize();
-        Vector3d liftForce=L*liftDirection;
-        Vector3d dragDirection= -(linVel-linVel.Dot(wingCut)).Normalize();
-        Vector3d dragForce=D*dragDirection;
-
-        this->base_link->AddForceAtRelativePosition(liftForce,this->force_pos);
-        this->base_link->AddForceAtRelativePosition(dragForce,this->force_pos);
-        */
 
 		//debuging loop
 		if(counter==DEBUG_CONST)
@@ -181,14 +199,12 @@ namespace gazebo
 
         	rotorfreq_pub_->Publish(msg);*/
 
-			gzdbg << timeStep <<std::endl;
+			gzdbg << rotorOmega/2.0/PI*60 <<std::endl;
 			gzdbg <<"R: "<< roll << std::endl;
 			gzdbg <<"P: "<< pitch << std::endl;
-            gzdbg <<"rychlost:" <<vel.Length()<<std::endl;
-
-            gzdbg << ToDeg(getFlapCorectionAngle(flaping_angle)) <<std::endl;
-
-            gzdbg << ToDeg(alpha)<<std::endl;
+            
+            gzdbg <<ToDeg(new_flap_angle)<<std::endl;
+           
 
 
             /*for(int u=-90;u<90;u++)
@@ -212,7 +228,12 @@ namespace gazebo
 		unsigned int leftBladeVisualID;
 		unsigned int rightBladeVisualID;
 		double blade_length;
+        double blade_width;
+        double blade_weight;
         double delta_angle;
+        double air_density;
+        int number_of_elements;
+        double element_area=this->blade_width*this->blade_length/this->number_of_elements;
 
 		Vector3d lbladeDefaultPos;
 		Vector3d rbladeDefaultPos;
@@ -226,6 +247,9 @@ namespace gazebo
 		//input
 		double roll;
 		double pitch;
+
+        //debug
+        double rotadd;
 
 		//messaging
 		transport::NodePtr node_handle;
@@ -250,7 +274,8 @@ namespace gazebo
 
 		void OnPitchCmdMsg(ConstAnyPtr &_msg)
 		{
-			pitch = _msg->double_value();
+			//pitch = _msg->double_value();
+            rotadd=_msg->double_value();
 		}
 
         double getFlapCorectionAngle(double flapping_angle)
@@ -260,16 +285,16 @@ namespace gazebo
             return std::atan2(rotated.Z(),1);   
         }
 
-       /* double getCL(double alpha)
+        double getCL(double alpha)
         {
             double alpha_degree=alpha*360.0/(2.0*3.141592);
             double CL=0.0;
-            if(alpha_degree > 0  && alpha_degree <= 10)
-                CL=(alpha_degree)*0.07;
-            if(alpha_degree > 10  && alpha_degree <= 35)
-                CL=(alpha_degree-10)*0.004+0.7;
-            if(alpha_degree > 35 && alpha_degree < 90)
-                CL=(alpha_degree-35)*-0.014+0.8;
+            if(alpha_degree > -10  && alpha_degree <= -5)
+                CL=-0.5;
+            if(alpha_degree > -5  && alpha_degree <= 7.5)
+                CL=alpha_degree*0.136+0.18;
+            if(alpha_degree > 7.5 && alpha_degree < 20)
+                CL=1.2;
             return CL;
         };
 
@@ -277,16 +302,20 @@ namespace gazebo
         {
             double alpha_degree=alpha*360.0/(2.0*3.141592);
             double CD=0.0;
-            if(alpha_degree > -180  && alpha_degree <= -90)
-                CD=(alpha_degree+180)/90*(-1.2);
-            if(alpha_degree > -90  && alpha_degree <= 90)
-                CD=alpha_degree/90*1.2;
-            if(alpha_degree > 90 && alpha_degree <= 180)
-                CD=(alpha_degree-90)/90*-1.2+1.2;
-            return fabs(CD);
+            if(alpha_degree > -10  && alpha_degree <= -5)
+                CD=alpha_degree*(-0.016)-0.06;
+            if(alpha_degree > -5  && alpha_degree <= 0)
+                CD=alpha_degree*(-0.002)+0.01;
+            if(alpha_degree > 0 && alpha_degree <= 7.5)
+                CD=0.01;
+            if(alpha_degree > 7.5 && alpha_degree <= 15)
+                CD=alpha_degree*0.008-0.05;
+            if(alpha_degree > 15 && alpha_degree <= 20)
+                CD=alpha_degree*0.2-2.3;
+            return CD;
         };
 
-        transport::NodePtr node_handle_;
+     /*   transport::NodePtr node_handle_;
         transport::PublisherPtr rotorfreq_pub_;*/
 
 
